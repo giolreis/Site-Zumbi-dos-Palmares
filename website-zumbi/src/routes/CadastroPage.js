9f5c6cf9b0d66256997a1b6ed124d4370aefeb89
Giovani Luiz dos Reis <50921714+giolreis@users.noreply.github.com>
2023-06-24 16:45:30 -0300

    atualização

--- website-zumbi/src/routes/CadastroPage.js
+++ website-zumbi/src/routes/CadastroPage.js
@@ -7,8 +7,8 @@ function CadastroPage() {
             <Secao
             cName="secao"
             imagem="https://images.unsplash.com/photo-1567122227018-ad02003c25fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGFmcmljYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60"
-            titulo='Título que vai na página Cadastro'
-            texto='texto sobre seus cadastro'
+            titulo='Junte-se à Nossa Comunidade: Cadastre-se Agora'
+            texto=''
             />
             <FormCadastro/>
 
