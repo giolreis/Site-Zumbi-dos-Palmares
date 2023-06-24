38b48d1c744230b3ff23d720c6362b4fcb414b6b
Giovani Luiz dos Reis <50921714+giolreis@users.noreply.github.com>
2023-06-24 17:15:44 -0300

    atualização 1.2

--- website-zumbi/src/routes/ContatoPage.js
+++ website-zumbi/src/routes/ContatoPage.js
@@ -7,7 +7,7 @@ function ContatosPage() {
             <Secao
             cName="secao"
             imagem="https://images.unsplash.com/photo-1550051414-003c9007794c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"
-            titulo='Entre em Contato : Juntos Pela Igualdade e Justiça'
+            titulo='Entre em Contato a gente : Juntos Pela Igualdade e Justiça'
             />
             <FormContato/>
         </>
